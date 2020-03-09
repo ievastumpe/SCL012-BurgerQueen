36d7743ce3483bfa37c27fbb7fe1ebd1bec501b5
NPGonzalez <naypili@gmail.com>
2020-03-09 17:30:12 -0300

    Se eliminó carpeta de imágenes fuera de assets

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('SCL012-Burger-Queen app is running!');
+    expect(page.getTitleText()).toEqual('spa app is running!');
   });
 
   afterEach(async () => {
