2946b862ac1a2b3988ed3bc6992991433e887aad
NPGonzalez <naypili@gmail.com>
2020-03-09 16:00:35 -0300

    Se agregan archivos del componente principal

---
+++ src/app/components/statusorders/statusorders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statusorders',
+  templateUrl: './statusorders.component.html',
+  styleUrls: ['./statusorders.component.css']
+})
+export class StatusordersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
