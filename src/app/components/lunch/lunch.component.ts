50fd5f360998766efb3441f43a300d722d535694
Ruth Maureira <ruthalemau@gmail.com>
2020-03-12 11:50:18 -0300

    modificación botón envío

--- src/app/components/lunch/lunch.component.ts
+++ src/app/components/lunch/lunch.component.ts
@@ -26,6 +26,7 @@ export class LunchComponent implements OnInit {
     console.log(event);
     this.clickProd = event.path[1].innerText.replace('Agregar', '');
     this.setList(this.clickProd);
+    
   }
 
   suma(event) {
@@ -36,4 +37,10 @@ export class LunchComponent implements OnInit {
     this.list.push(item);
     
   }
+  
+  saveLocal(array){
+    localStorage.setItem('datos', JSON.stringify(array));
+  }
+
+  
 }
