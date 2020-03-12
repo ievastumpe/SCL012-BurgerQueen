a855fff39a53e90cc687bf0942be5b4c061a1fac
Ruth Maureira <ruthalemau@gmail.com>
2020-03-12 10:23:03 -0300

    print desayuno - onclick

--- src/app/components/breakfast/breakfast.component.ts
+++ src/app/components/breakfast/breakfast.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
-import { BreakfastService, Breakfastb } from 'src/app/services/breakfast.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Router
+} from '@angular/router';
+import {
+  BreakfastService,
+  Breakfastb
+} from 'src/app/services/breakfast.service';
 
 @Component({
   selector: 'app-breakfast',
@@ -11,10 +19,10 @@ export class BreakfastComponent implements OnInit {
   clickProd: any;
   clickPrecio: number;
   list: any = [];
-  breakfast: Breakfastb[]=[];
-  constructor(private _breakfastService:BreakfastService,
-              private router:Router
-    ) {
+  breakfast: Breakfastb[] = [];
+  constructor(private _breakfastService: BreakfastService,
+    private router: Router
+  ) {
     // console.log("constructor");
   }
   ngOnInit(): void {
@@ -22,8 +30,14 @@ export class BreakfastComponent implements OnInit {
 
     this.breakfast = this._breakfastService.getBreakfast();
   }
-  verBreakfastb(idx:number){
-    // console.log(idx);
-    this.router.navigate( ['/breakfast', idx] );
+  onClick(event) {
+    console.log(event);
+    this.clickProd = event.path[1].innerText.replace('Agregar', '');
+    this.setList(this.clickProd);
+  }
+
+  setList(item: any) {
+    this.list.push(item);
+
   }
 }
